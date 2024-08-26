c884f9af300f323a10aeda959d18d0a77ed78605
skeletonk10 <poohbilly2@naver.com>
2024-08-26 14:02:22 +0900

    b-add: /user domain initialization

---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import bcrypt from 'bcrypt';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  nickname: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  private beforeInsert() {
+    this.password = bcrypt.hashSync(this.password, 10);
+  }
+}
