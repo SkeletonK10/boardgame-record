0d9963669e8289e7e831cb4f12144501ad7819be
skeletonk10 <poohbilly2@naver.com>
2024-08-26 15:49:09 +0900

    b-add: POST /user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const userEntity = await this.userRepository.create(createUserDto);
+    await this.userRepository.save(userEntity);
+    return {
+      code: 'OK',
+      msg: 'User has been succesfully created',
+    };
   }
 
   findAll() {
