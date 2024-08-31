2d206130f76647484144b61cfbda414ec7e72b1f
skeletonk10 <poohbilly2@naver.com>
2024-09-01 03:29:51 +0900

    f-add: /mahjong: designed record and ranking

--- frontend/src/app/mahjong/page.tsx
+++ frontend/src/app/mahjong/page.tsx
@@ -1,28 +1,80 @@
-"use client";
+import { api } from "@/lib/axiosInterceptor";
 import {
   Box,
   Grid2 as Grid,
   List,
   ListItem,
   ListItemButton,
-  styled,
   Typography,
 } from "@mui/material";
+import { MahjongMainPageDto } from "./dto";
 
-const ListBox = styled(Box)(({ theme }) => ({
-  display: "flex",
-  flexGrow: 1,
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+const testRecord = [
+  {
+    east: { nickname: "참가자1", score: 73000 },
+    south: { nickname: "참가자2", score: -23000 },
+    west: { nickname: "참가자3", score: 25000 },
+    north: { nickname: "참가자4", score: 25000 },
+  },
+  {
+    east: { nickname: "참가자2", score: 73000 },
+    south: { nickname: "참가자1", score: -23000 },
+    west: { nickname: "참가자3", score: 25000 },
+    north: { nickname: "참가자4", score: 25000 },
+  },
+];
 
-export default function MahjongMainPage() {
+const testRanking = [
+  {
+    nickname: '참가자1',
+    score: 102.3,
+  },
+  {
+    nickname: '참가자2',
+    score: 12.3,
+  },
+  {
+    nickname: '참가자1',
+    score: -2.3,
+  },
+  {
+    nickname: '참가자1',
+    score: -60.3,
+  },
+];
+
+const getProps: () => Promise<MahjongMainPageDto> = async () => {
+  try {
+    const recordResponsePromise = api.get(`/mahjong/record`);
+    const rankingResponsePromise = api.get(`/mahjong/ranking`);
+    const [recordResponse, rankingResponse] = await Promise.all([recordResponsePromise, rankingResponsePromise]);
+    const record = (recordResponse.data as any).data.slice(10);
+    const ranking = (rankingResponse.data as any).data;
+    return {
+      record,
+      ranking,
+    }
+  } catch (err) {
+    // return test data
+    return {
+      record: testRecord,
+      ranking: testRanking,
+    }
+  }
+}
+
+export default async function MahjongMainPage() {
+  const { record, ranking } = await getProps();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={1}>
         <Grid size={{ xs: 12, sm: 6 }}>
-          <ListBox>
+          <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
             <Typography
               variant="h6"
               noWrap
@@ -34,42 +86,74 @@ export default function MahjongMainPage() {
             >
               최근 기록
             </Typography>
-            <List>
-              {[
-                {
-                  east: { nickname: "참가자1", score: 73000 },
-                  south: { nickname: "참가자2", score: -23000 },
-                  west: { nickname: "참가자3", score: 25000 },
-                  north: { nickname: "참가자4", score: 25000 },
-                },
-              ].map((value, index) => (
+            <List sx={{width: '100%'}}>
+              {record.map((value, index) => (
                 <ListItem key={index}>
                   <ListItemButton>
-                    <Grid container>
+                    <Grid container spacing={1} sx={{width: '100%'}}>
                       <Grid size={6}>
-                        <Box>{value.east.nickname}</Box>
-                        <Box>{value.east.score}</Box>
+                        <Typography component="div" noWrap>{value.east.nickname}</Typography>
+                        <Typography component="div">{value.east.score}</Typography>
                       </Grid>
                       <Grid size={6}>
-                        <Box>{value.south.nickname}</Box>
-                        <Box>{value.south.score}</Box>
+                        <Typography component="div" noWrap>{value.south.nickname}</Typography>
+                        <Typography component="div">{value.south.score}</Typography>
                       </Grid>
                       <Grid size={6}>
-                        <Box>{value.west.nickname}</Box>
-                        <Box>{value.west.score}</Box>
+                        <Typography component="div" noWrap>{value.west.nickname}</Typography>
+                        <Typography component="div">{value.west.score}</Typography>
                       </Grid>
                       <Grid size={6}>
-                        <Box>{value.north.nickname}</Box>
-                        <Box>{value.north.score}</Box>
+                        <Typography component="div" noWrap>{value.north.nickname}</Typography>
+                        <Typography component="div">{value.north.score}</Typography>
                       </Grid>
                     </Grid>
                   </ListItemButton>
                 </ListItem>
               ))}
             </List>
-          </ListBox>
+          </Box>
         </Grid>
-        <Grid size={{ xs: 12, sm: 6 }}>abcd</Grid>
+        <Grid size={{ xs: 12, sm: 6 }}>
+          <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{
+                display: { sm: "block" },
+                userSelect: "none",
+                flexGrow: 1
+              }}
+            >
+              우마 순위
+            </Typography>
+            <List sx={{width: '100%'}}>
+              {ranking.map((value, index) => (
+                <ListItem key={index}>
+                  <ListItemButton>
+                    <Grid container spacing={1} sx={{width: '100%'}}>
+                      <Grid size={2}>
+                        <Box>{index+1}</Box>
+                      </Grid>
+                      <Grid size={6}>
+                        <Typography component="div" noWrap>{value.nickname}</Typography>
+                      </Grid>
+                      <Grid size={4}>
+                        <Box>{value.score}</Box>
+                      </Grid>
+                    </Grid>
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Grid>
       </Grid>
     </Box>
   );
