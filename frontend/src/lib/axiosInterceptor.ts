ed82571cbfce9de94960bf2ba994c412ee3b8d40
skeletonk10 <poohbilly2@naver.com>
2024-09-02 14:26:57 +0900

    f-add: axios refresh logic (WITHOUT TEST)

--- frontend/src/lib/axiosInterceptor.ts
+++ frontend/src/lib/axiosInterceptor.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
+const REFRESH_URL = `/auth/refresh`;
+
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
@@ -13,3 +15,25 @@ api.interceptors.request.use((config) => {
   };
   return config;
 });
+
+api.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const {
+      config,
+      response: { status },
+    } = err;
+
+    if (config.url === REFRESH_URL || status !== 401 || config.sent) {
+      return Promise.reject(err);
+    }
+    const { access } = (await api.post(REFRESH_URL)).data as any;
+    if (access) {
+      cookies().set("access", access);
+      config.headers = {
+        authorization: access,
+      };
+    }
+    return api(config);
+  }
+);
