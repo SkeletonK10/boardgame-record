6c60ba68a0aa5b21c2e492141fd64da0bc299281
skeletonk10 <poohbilly2@naver.com>
2024-09-13 15:22:22 +0900

    f-fix: proper logout

--- frontend/src/app/appbar.tsx
+++ frontend/src/app/appbar.tsx
@@ -79,8 +79,7 @@ export default function AppBar() {
                   noWrap
                   component="div"
                   onClick={() => {
-                    signOut();
-                    router.refresh();
+                    signOut({ redirect: true, callbackUrl: `/` });
                   }}
                   sx={{
                     padding: '0.5rem',
