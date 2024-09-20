81c4b786aa7d21a97dc82dbeadeddf5c1162b68b
skeletonk10 <poohbilly2@naver.com>
2024-09-21 01:52:52 +0900

    f-mod: getting easy to see stats

--- frontend/src/app/mahjong/statistics/player/page.tsx
+++ frontend/src/app/mahjong/statistics/player/page.tsx
@@ -8,19 +8,19 @@ import { text } from "@/lib/data";
 
 const columns: GridColDef[] = [
     // { field: 'playerName', headerName: '아이디' },       // 필요 없을듯?
-    { field: 'nickname', headerName: '이름', width: 100 },
-    { field: 'count', headerName: '대국수', type: 'number', width: 130 },
+    { field: 'nickname', headerName: '이름', width: 70 },
     // { field: 'category', headerName: '카테고리' },       // 이미 카테고리는 나누어져 있음
-    { field: 'rating', headerName: '우마 총합', type: 'number', width: 130 },
-    { field: 'averageRating', headerName: '평균 우마', type: 'number', width: 130 },
-    { field: 'averageScore', headerName: '평균 점수', type: 'number', width: 130 },
-    { field: 'maxScore', headerName: '최고 점수', type: 'number', width: 130 },
-    { field: 'minScore', headerName: '최소 점수', type: 'number', width: 130 },
-    { field: 'first', headerName: '1등', type: 'number', width: 100 },
-    { field: 'second', headerName: '2등', type: 'number', width: 100 },
-    { field: 'third', headerName: '3등', type: 'number', width: 100 },
-    { field: 'fourth', headerName: '4등', type: 'number', width: 100 },
-    { field: 'tobi', headerName: '토비', type: 'number', width: 100 },
+    { field: 'rating', headerName: '우마 총합', type: 'number', width: 70 },
+    { field: 'averageRating', headerName: '평균 우마', type: 'number', width: 70 },
+    { field: 'averageScore', headerName: '평균 점수', type: 'number', width: 80 },
+    { field: 'maxScore', headerName: '최고 점수', type: 'number', width: 80 },
+    { field: 'minScore', headerName: '최소 점수', type: 'number', width: 80 },
+    { field: 'count', headerName: '대국수', type: 'number', width: 70 },
+    { field: 'first', headerName: '1등', type: 'number', width: 75 },
+    { field: 'second', headerName: '2등', type: 'number', width: 75 },
+    { field: 'third', headerName: '3등', type: 'number', width: 75 },
+    { field: 'fourth', headerName: '4등', type: 'number', width: 75 },
+    { field: 'tobi', headerName: '토비', type: 'number', width: 75 },
 ]
 
 
@@ -66,7 +66,7 @@ export default function MahjongPlayerStatisticsPage() {
         {text.mahjong.statistics.player.subtitle}
       </Typography>
       <RadioGroup row onChange={(e, v) => setCategory(v)}>
-        <FormControlLabel  value="3마" control={<Radio />} label="3마" />
+        <FormControlLabel value="3마" control={<Radio />} label="3마" />
         <FormControlLabel value="4마" control={<Radio />} label="4마" />
       </RadioGroup>
       {
@@ -78,7 +78,16 @@ export default function MahjongPlayerStatisticsPage() {
             <DataGrid
               rows={stats}
               columns={columns}
-              sx={{ width: '100%' }}
+              sx={{
+                width: "100%",
+                
+                "& .MuiDataGrid-columnHeaderTitle": {
+                  whiteSpace: "normal",
+                  lineHeight: "normal"
+                },
+              }}
+              disableColumnMenu
+              columnHeaderHeight={90}
             />
           )
       }
