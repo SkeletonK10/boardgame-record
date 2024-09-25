7cb53733e2dc403167efb4bfffc8fc01e9013e35
skeletonk10 <poohbilly2@naver.com>
2024-09-25 14:35:34 +0900

    f-add: patch note to main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
 import { text } from "@/lib/data";
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
+
+import PatchNote from "../lib/patchNote.mdx";
 
 export default function Home() {
   return (
     <Box>
       <h2>{text.main.title}</h2>
       <h3>{text.main.developing}</h3>
+      <Divider />
+      <PatchNote />
     </Box>
   );
 }
