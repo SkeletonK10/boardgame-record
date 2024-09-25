daff4acf57fb7aa551fcddff417733176ecee3f2
skeletonk10 <poohbilly2@naver.com>
2024-09-25 17:16:23 +0900

    b-add: GET /mahjong/:id

--- backend/src/mahjong/mahjong.controller.ts
+++ backend/src/mahjong/mahjong.controller.ts
@@ -59,6 +59,25 @@ export class MahjongController {
     }
   }
 
+  @Get(':id')
+  async findOneById(@Param('id') id: number) {
+    try {
+      const res = await this.mahjongService.findById(+id);
+      return {
+        code: `OK`,
+        msg: `${id}번 마작 경기 상세`,
+        data: res,
+      };
+    } catch (err) {
+      const code =
+        err instanceof Error ? err.message : `ERROR_MAHJONG_GAME_FINDONE`;
+      return {
+        code: code,
+        msg: `알 수 없는 에러가 발생했습니다.`,
+      };
+    }
+  }
+
   // TODO: PATCH /:id
   // Needs 'Mahjong Record Admin' Role
   // Body: {east: {nickname, score}, south, west, north}
