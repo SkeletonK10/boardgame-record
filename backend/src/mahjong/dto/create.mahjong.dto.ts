1e4c19cd93be3dd485232bbf35a97d0782f36fcd
skeletonk10 <poohbilly2@naver.com>
2024-10-11 01:00:46 +0900

    b-mod: add yakuman dto to CreateMahjongDto

--- backend/src/mahjong/dto/create.mahjong.dto.ts
+++ backend/src/mahjong/dto/create.mahjong.dto.ts
@@ -1,12 +1,27 @@
 import { MahjongSubcategory } from '../constants/mahjong.constant';
 
-class MahjongPlay {
+class PlayerRecordDto {
   playerName: string;
   isGuest: boolean;
   score: number;
 }
+
+class YakumanRecordDto {
+  // 역만 종류
+  yakuman: string[];
+  // 화료자
+  winner: string;
+  // 방총자 (쯔모 시엔 null)
+  opponent: string | null;
+  // 쯔모했나요?
+  tsumo: boolean;
+  // 동 n국 m본장 (모르면 null)
+  round: string | null;
+}
+
 export class CreateMahjongGameDto {
   subcategory: MahjongSubcategory;
-  players: MahjongPlay[];
+  players: PlayerRecordDto[];
+  yakumans: YakumanRecordDto[];
   note?: string;
 }
