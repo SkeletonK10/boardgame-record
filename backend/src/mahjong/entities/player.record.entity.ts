65f292e2377787abde96cb301ef24e28adf474df
skeletonk10 <poohbilly2@naver.com>
2024-10-11 00:18:31 +0900

    b-add: entity for mahjong yakuman record

--- backend/src/mahjong/entities/player.record.entity.ts
+++ backend/src/mahjong/entities/player.record.entity.ts
@@ -3,11 +3,13 @@ import {
   DeleteDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { MahjongPlayer } from '../player/entities/player.entity';
 import { MahjongGameRecord } from './game.record.entity';
 import { MahjongSeat } from '../constants/mahjong.constant';
+import { MahjongYakumanRecord } from './yakuman.record.entity';
 
 @Entity()
 export class MahjongPlayerRecord {
@@ -19,6 +21,9 @@ export class MahjongPlayerRecord {
 
   @ManyToOne(() => MahjongPlayer, (player) => player.games)
   player: MahjongPlayer;
+
+  @OneToMany(() => MahjongYakumanRecord, (yakuman) => yakuman.record)
+  yakumans: MahjongYakumanRecord;
 
   @Column()
   seat: MahjongSeat;
