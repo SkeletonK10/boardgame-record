cc1a65991f3e0b0cb20f989e0ae602a251e35b12
skeletonk10 <poohbilly2@naver.com>
2024-10-11 00:18:31 +0900

    b-add: entity for mahjong yakuman record

--- backend/src/mahjong/constants/mahjong.constant.ts
+++ backend/src/mahjong/constants/mahjong.constant.ts
@@ -14,3 +14,24 @@ export type MahjongCategory = (typeof MahjongCategoryValues)[number];
 export const MahjongSubcategoryValues = ['반장전', '동풍전'] as const;
 
 export type MahjongSubcategory = (typeof MahjongSubcategoryValues)[number];
+
+export const MahjongYakumanValues = [
+  '헤아림 역만',
+  '천화',
+  '지화',
+  '스안커',
+  '스안커 단기',
+  '국사무쌍',
+  '국사무쌍 13면 대기',
+  '구련보등',
+  '순정구련보등',
+  '녹일색',
+  '자일색',
+  '청노두',
+  '대삼원',
+  '소사희',
+  '대사희',
+  '스깡즈',
+] as const;
+
+export type MahjongYakuman = (typeof MahjongYakumanValues)[number];
---
+++ backend/src/mahjong/entities/yakuman.record.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { MahjongYakuman } from '../constants/mahjong.constant';
+import { MahjongPlayerRecord } from './player.record.entity';
+
+@Entity()
+export class MahjongYakumanRecord {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => MahjongPlayerRecord, (record) => record.yakumans)
+  record: MahjongPlayerRecord;
+
+  @Column({ array: true })
+  yakuman: MahjongYakuman[];
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
