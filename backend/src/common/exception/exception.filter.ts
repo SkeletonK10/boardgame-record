f2fc589238cf433ac9c2b76e706055d137c1b046
skeletonk10 <poohbilly2@naver.com>
2024-11-03 17:03:15 +0900

    b-add: error message for dev env

--- backend/src/common/exception/exception.filter.ts
+++ backend/src/common/exception/exception.filter.ts
@@ -3,17 +3,23 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  Inject,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private readonly configService: ConfigService) {}
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
     const message = exception.message;
+
+    if (this.configService.get('IS_DEV')) console.error(message);
 
     response.status(status).json({
       statusCode: status,
