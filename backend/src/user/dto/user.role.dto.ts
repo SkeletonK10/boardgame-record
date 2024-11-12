db8c6f777b614ffa34ddd7b777fd6f2ba9ffbd97
skeletonk10 <poohbilly2@naver.com>
2024-11-12 13:18:47 +0900

    b-mod: compatible dto with swagger

--- backend/src/user/dto/user.role.dto.ts
+++ backend/src/user/dto/user.role.dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { RoleType } from 'src/user/entities/role.entity';
 
 export class UserRoleDto {
+  @ApiProperty()
   role: RoleType;
+
+  @ApiProperty()
   username: string;
 }
