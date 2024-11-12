0e63869844e66d922d30ad082331658a179eaddd
skeletonk10 <poohbilly2@naver.com>
2024-11-12 14:08:43 +0900

    b-add: response format (except mahjongController)

--- backend/src/mahjong/player/player.controller.ts
+++ backend/src/mahjong/player/player.controller.ts
@@ -15,6 +15,7 @@ import { RoleGuard, Roles } from 'src/auth/guard/role.guard';
 import { Role } from 'src/user/entities/role.entity';
 import { MahjongCategory } from '../constants/mahjong.constant';
 import { ServiceException } from 'src/common/exception/exception';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('mahjong/player')
 export class MahjongPlayerController {
@@ -23,6 +24,9 @@ export class MahjongPlayerController {
   @UseGuards(JwtAccessTokenGuard, RoleGuard)
   @Roles(Role.ADMIN, Role.MAHJONG_RECORD_ADMIN)
   @Post()
+  @ApiCreatedResponse({
+    description: 'Player successfuly created',
+  })
   async create(@Body() createMahjongPlayerDto: CreateMahjongPlayerDto) {
     return await this.MahjongplayerService.create(createMahjongPlayerDto);
   }
@@ -31,17 +35,42 @@ export class MahjongPlayerController {
   @Roles(Role.ADMIN)
   @HttpCode(200)
   @Post('guest-count')
+  @ApiOkResponse({
+    description: 'Number of players using corresponding playerName',
+    example: 3,
+  })
   async countGuestCount(@Body('playerName') playerName: string) {
     return await this.MahjongplayerService.countGuestByPlayerName(playerName);
   }
 
   @Get()
+  @ApiOkResponse({
+    example: [
+      {
+        playerName: '홍길동A',
+        nickname: '홍길동',
+        rating: [
+          { category: '4마', rating: 23.5 },
+          { category: '3마', rating: -222.1 },
+        ],
+      },
+    ],
+  })
   async getAll() {
     const res = await this.MahjongplayerService.getAll();
     return res;
   }
 
   @Get('/:playername/info')
+  @ApiOkResponse({
+    example: {
+      id: 223123,
+      playerName: '홍길동A',
+      nickname: '홍길동',
+      createdAt: '2024-09-14T22:11:48.964Z',
+      updatedAt: '2024-09-14T22:11:48.964Z',
+    },
+  })
   async findOne(@Param('playername') playerName: string) {
     const res = await this.MahjongplayerService.findOneByPlayerName(playerName);
     if (!res) throw new ServiceException('MAHJONG_GAME_PLAYER_DOES_NOT_EXISTS');
@@ -49,6 +78,28 @@ export class MahjongPlayerController {
   }
 
   @Get('ranking')
+  @ApiOkResponse({
+    example: [
+      {
+        playerName: '김철수A',
+        nickname: '김철수',
+        rating: '152.60',
+        ranking: '1',
+      },
+      {
+        playerName: '김철수B',
+        nickname: '김철수',
+        rating: '152.60',
+        ranking: '1',
+      },
+      {
+        playerName: '홍길동A',
+        nickname: '홍길동',
+        rating: '150.00',
+        ranking: '3',
+      },
+    ],
+  })
   async getRanking(@Query('category') category: MahjongCategory) {
     const res = await this.MahjongplayerService.getRanking(category);
     return res;
