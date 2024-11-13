e00de64241c3c0d21c28e3238ebdbe9577e0c49d
skeletonk10 <poohbilly2@naver.com>
2024-11-13 16:39:44 +0900

    b-add: response example for GET /mahjong/game

--- backend/src/mahjong/mahjong.controller.ts
+++ backend/src/mahjong/mahjong.controller.ts
@@ -23,14 +23,57 @@ export class MahjongController {
   @UseGuards(JwtAccessTokenGuard, RoleGuard)
   @Roles(Role.MAHJONG_RECORD_ADMIN, Role.ADMIN)
   @Post('game')
-  @ApiCreatedResponse()
+  @ApiCreatedResponse({
+    example: {
+      id: 345,
+    },
+  })
   async createGame(@Body() createMahjongGameDto: CreateMahjongGameDto) {
     const res = await this.mahjongService.create(createMahjongGameDto);
     return { id: res.id };
   }
 
   @Get('game')
-  @ApiOkResponse()
+  @ApiOkResponse({
+    example: [
+      {
+        id: 213,
+        subcategory: '반장전',
+        category: '4마',
+        players: [
+          {
+            playerName: '김철수A',
+            nickname: '김철수',
+            seat: 0,
+            rank: 3,
+            score: 25000,
+          },
+          {
+            playerName: '김영희A',
+            nickname: '김영희',
+            seat: 1,
+            rank: 1,
+            score: 37200,
+          },
+          {
+            playerName: '홍길동A',
+            nickname: '홍길동',
+            seat: 2,
+            rank: 4,
+            score: 11000,
+          },
+          {
+            playerName: '홍길순A',
+            nickname: '홍길순',
+            seat: 3,
+            rank: 2,
+            score: 26800,
+          },
+        ],
+        note: null,
+      },
+    ],
+  })
   async findAll(
     @Query('playername') playerName?: string,
     @Query('category') category?: MahjongCategory,
