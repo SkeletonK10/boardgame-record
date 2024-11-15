2a3e7bb0d0aa6c8852f376bc39b7e08058fa182b
skeletonk10 <poohbilly2@naver.com>
2024-11-15 16:18:27 +0900

    b-mod: activate GET /mahjong/game/:id

--- backend/src/mahjong/mahjong.controller.ts
+++ backend/src/mahjong/mahjong.controller.ts
@@ -43,24 +43,11 @@ export class MahjongController {
     return res;
   }
 
-  // @Get('game/:id')
-  // async findOneById(@Param('id') id: number) {
-  //   try {
-  //     const res = await this.mahjongService.findById(+id);
-  //     return {
-  //       code: `OK`,
-  //       msg: `${id}번 마작 경기 상세`,
-  //       data: res,
-  //     };
-  //   } catch (err) {
-  //     const code =
-  //       err instanceof Error ? err.message : `ERROR_MAHJONG_GAME_FINDONE`;
-  //     return {
-  //       code: code,
-  //       msg: `알 수 없는 에러가 발생했습니다.`,
-  //     };
-  //   }
-  // }
+  @Get('game/:id')
+  async findOneById(@Param('id') id: number) {
+    const res = await this.mahjongService.findById(+id);
+    return res;
+  }
 
   // TODO: PATCH /:id
   // Needs 'Mahjong Record Admin' Role
