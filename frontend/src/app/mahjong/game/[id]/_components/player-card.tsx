40e246fb154f56e3166ca3672f8b953bab48fee8
skeletonk10 <poohbilly2@naver.com>
2025-02-10 16:56:26 +0900

    f-mod: design for player card

--- frontend/src/app/mahjong/game/[id]/_components/player-card.tsx
+++ frontend/src/app/mahjong/game/[id]/_components/player-card.tsx
@@ -5,19 +5,36 @@ interface MahjongPlayerCardProps {
   playerName: string;
   nickname: string;
   score: number;
+  rank: number;
   ratingDiff: number;
 }
+
+const paperStyle = {
+  backgroundColor: "#2F4F4F",
+};
 
 const style = { fontSize: "0.8rem", textAlign: "center" };
 
 const ratingDiffStyle = (ratingDiff: number) => {
   let color = "#000000";
-  if (ratingDiff > 0) color = "#81C147"; // 194, 63, 56
-  else if (ratingDiff < 0) color = "#C23F38";
+  if (ratingDiff > 0) color = "#4B8B3B";
+  else if (ratingDiff < 0) color = "#8B0000";
   return {
     fontSize: "0.7rem",
     textAlign: "center",
     color,
+  };
+};
+
+const backgroundStyle = (rank: number) => {
+  let color = "#FFFFFF";
+  if (rank === 1) color = "#FFD700";
+  else if (rank === 2) color = "#C0C0C0";
+  else if (rank === 3) color = "#CD7F32";
+  return {
+    fontSize: "0.7rem",
+    textAlign: "center",
+    backgroundColor: color,
   };
 };
 
@@ -30,13 +47,17 @@ export default function PlayerCard({
   playerName,
   nickname,
   score,
+  rank,
   ratingDiff,
 }: MahjongPlayerCardProps) {
   return (
-    <Paper>
+    <Paper sx={backgroundStyle(rank)}>
       <Grid container>
         <Grid size={12}>
-          <Typography sx={style}>{`${nickname} (${playerName})`}</Typography>
+          <Typography
+            sx={style}
+            noWrap
+          >{`${nickname} (${playerName})`}</Typography>
         </Grid>
         <Grid size={6}>
           <Typography sx={style}>{score}</Typography>
